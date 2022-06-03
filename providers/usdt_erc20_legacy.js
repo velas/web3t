9fa05c81846d6bff1b40d5fc4d70adc63e4b2b78
FrozenPyrozen <kiril.kohan@gmail.com>
2022-06-03 15:01:43 +0300

    added txs limit for `usdt_erc20_legacy`

--- providers/usdt_erc20_legacy.js
+++ providers/usdt_erc20_legacy.js
@@ -293,6 +293,8 @@
     endblock = 99999999;
     sort = "asc";
     apikey = "4TNDAGS373T78YJDYBFH32ADXPVRMXZEIG";
+    const page = 1;
+    const offset = 30;
     query = stringify({
       module: module,
       action: action,
@@ -301,6 +303,8 @@
       sort: sort,
       startblock: startblock,
       endblock: endblock,
+      page,
+      offset,
     });
     return get(apiUrl + "?" + query)
       .timeout({
